4df7c1242629c89ecc9d7f47b120d898dd69f4d8
baymeric <aymeric.beaune@ynov.com>
2025-12-01 08:47:40 +0100

    [ :white_check_mark: ] - Modification index.html

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"html/template"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
-	"strings"
 )
 
 type PageData struct {
@@ -85,43 +85,83 @@ func (app *PageData) DisplayPage(templateName string) http.HandlerFunc {
 	}
 }
 
-func (app *PageData) DisplayArtist() http.HandlerFunc {
+func (app *PageData) DisplayArtist(GetRandom bool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if GetRandom {
+			idStr := fmt.Sprint(rand.Intn(len(app.Groups)))
 
-		idStr := r.URL.Query().Get("id")
-		if idStr == "" {
-			http.Error(w, "Missing id", http.StatusBadRequest)
-			return
-		}
-
-		var id int
-		fmt.Sscanf(idStr, "%d", &id)
-
-		var found *Group
-		for _, g := range app.Groups {
-			if g.ID == id {
-				found = &g
-				break
+			if idStr == "" {
+				http.Error(w, "Missing id", http.StatusBadRequest)
+				return
 			}
-		}
 
-		if found == nil {
-			http.NotFound(w, r)
-			return
-		}
+			var id int
+			fmt.Sscanf(idStr, "%d", &id)
 
-		data := PageArtist{
-			Artist:   *found,
-			Settings: app.Settings,
-		}
+			var found *Group
+			for _, g := range app.Groups {
+				if g.ID == id {
+					found = &g
+					break
+				}
+			}
 
-		tmpl, err := template.ParseFiles("./static/html/artist.html")
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
+			if found == nil {
+				http.NotFound(w, r)
+				return
+			}
 
-		tmpl.Execute(w, data)
+			data := PageArtist{
+				Artist:   *found,
+				Settings: app.Settings,
+			}
+
+			tmpl, err := template.ParseFiles("./static/html/index.html")
+
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+
+			tmpl.Execute(w, data)
+		} else {
+			idStr := r.URL.Query().Get("id")
+
+			if idStr == "" {
+				http.Error(w, "Missing id", http.StatusBadRequest)
+				return
+			}
+
+			var id int
+			fmt.Sscanf(idStr, "%d", &id)
+
+			var found *Group
+			for _, g := range app.Groups {
+				if g.ID == id {
+					found = &g
+					break
+				}
+			}
+
+			if found == nil {
+				http.NotFound(w, r)
+				return
+			}
+
+			data := PageArtist{
+				Artist:   *found,
+				Settings: app.Settings,
+			}
+
+			tmpl, err := template.ParseFiles("./static/html/artist.html")
+
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+
+			tmpl.Execute(w, data)
+		}
 	}
 }
 
@@ -140,25 +180,12 @@ func main() {
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-	http.HandleFunc("/", app.DisplayPage("index"))
+	http.HandleFunc("/", app.DisplayArtist(true))
 	http.HandleFunc("/artists", app.DisplayPage("artists"))
 	http.HandleFunc("/concerts", app.DisplayPage("concerts"))
 	http.HandleFunc("/map", app.DisplayPage("map"))
 	http.HandleFunc("/about", app.DisplayPage("about"))
-	http.HandleFunc("/artist", app.DisplayArtist())
-
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		query := r.URL.Query().Get("q")
-		var filtered []Group
-		for _, g := range app.Groups {
-			if strings.Contains(strings.ToLower(g.Name), strings.ToLower(query)) {
-				filtered = append(filtered, g)
-			}
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(filtered)
-	})
+	http.HandleFunc("/artist", app.DisplayArtist(false))
 
 	log.Println("Serveur lancé sur http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
